5281f946a77e234cb917691e19c9abbd69965a98
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2020-03-11 13:09:06 +0100

    [WIP] use nhash to generate luid

--- sys/luid/luid.c
+++ sys/luid/luid.c
@@ -30,19 +30,36 @@
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 #endif
 
+static void _nhash(const void *_in, size_t in_len,
+                   void *_out, size_t out_len)
+{
+    const uint8_t *in = _in;
+    uint8_t *out = _out;
+
+    static const uint8_t primes[] = { 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+                                     43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113};
+
+    uint32_t sum = 0xAA;
+    size_t iterations = MAX(in_len, out_len);
+    for (size_t i = 0; i < iterations; ++i) {
+        uint8_t c = in[i % in_len];
+        sum += c * primes[c % ARRAY_SIZE(primes)] + 1;
+        out[i % out_len] = sum & 0xFF;
+    }
+}
+
 void __attribute__((weak)) luid_base(void *buf, size_t len)
 {
     memset(buf, LUID_BACKUP_SEED, len);
 
 #if CPUID_LEN
-    uint8_t *out = (uint8_t *)buf;
-    uint8_t cid[CPUID_LEN];
-
-    cpuid_get(cid);
-    for (size_t i = 0; i < MAX(len, CPUID_LEN); i++) {
-        out[i % len] ^= cid[i % CPUID_LEN];
-    }
+    uint8_t in[CPUID_LEN];
+    cpuid_get(in);
+#else
+    uint8_t in[] = { LUID_BACKUP_SEED };
 #endif
+
+    _nhash(&in, sizeof(in), buf, len);
 }
 
 static uint8_t lastused;
