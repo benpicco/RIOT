5440f4106b3c3dba291d49029be6bd08211f19d7
Daniel Lockau <daniel.lockau@ml-pa.com>
2021-03-20 08:51:12 +0100

    sam0_common/periph/adc: include hack for lbc_at-v9

--- cpu/sam0_common/periph/adc.c
+++ cpu/sam0_common/periph/adc.c
@@ -73,7 +73,10 @@ static inline void _wait_syncbusy(void)
      * The ADC SYNCBUSY.SWTRIG gets stuck to '1' after wake-up from Standby Sleep mode.
      * SAMD5x/SAME5x errata: DS80000748 (page 10)
      */
-    while (ADC_DEV->SYNCBUSY.reg & ~ADC_SYNCBUSY_SWTRIG) {}
+    do {
+        /* XXX: Why do we need to wait before accessing SYNCBUSY after hibernate? */
+        for (volatile unsigned int i = 0; i < 50; i++) {}
+    } while (ADC_DEV->SYNCBUSY.reg & ~ADC_SYNCBUSY_SWTRIG);
 #endif
 }
 
