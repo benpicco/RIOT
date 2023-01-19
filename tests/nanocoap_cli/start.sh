fb0d8911aa70e8838e4b3d1f495406a6295434ee
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2023-01-19 01:52:17 +0100

    tests/nanocoap_cli: tweak auto-start script

--- tests/nanocoap_cli/start.sh
+++ tests/nanocoap_cli/start.sh
@@ -2,24 +2,28 @@
 
 reset
 
+export USE_ZEP=1
+
 case $1 in
-root)
-	make PORT=tap_a0 all term
+0 | root)
+	TERMFLAGS="--id=1" make PORT=tap_a0 ZEP_MAC=AA:AA:AA:AA:AA:AA:AA:AA all term
 	;;
-a2)
-	echo "server start" | make PORT=tap_a2 all term
+1 | a2)
+	make PORT=tap_a2 ZEP_MAC=A2:A2:A2:A2:A2:A2:A2:A2 all term
 	;;
-b0a1)
-	echo "server start" | make PORT="tap_b0 tap_a1" all term
+2 | b0a1)
+	make PORT="tap_b0 tap_a1" ZEP_MAC=BB:BB:BB:BB:BB:BB:BB:BB all term
 	;;
-b2)
-	echo "server start" | make PORT=tap_b2 all term
+3 | b2)
+	make PORT=tap_b2 ZEP_MAC=B2:B2:B2:B2:B2:B2:B2:B2 all term
 	;;
-c0b1)
-	echo "server start" | make PORT="tap_c0 tap_b1" all term
+4 | c0b1)
+	make PORT="tap_c0 tap_b1" ZEP_MAC=CC:CC:CC:CC:CC:CC:CC:CC all term
 	;;
-c1)
-	echo "server start" | make PORT=tap_c1 all term
+5 | c2)
+	make PORT=tap_c2 ZEP_MAC=C2:C2:C2:C2:C2:C2:C2:C2 all term
 	;;
+zep)
+    ../../dist/tools/zep_dispatch/bin/zep_dispatch -t network.topo ::1 17754
 esac
 
