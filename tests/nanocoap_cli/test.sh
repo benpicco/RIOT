9e049544404f6b7359c7a35a4460b73a08de3a1b
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2023-10-04 16:29:00 +0200

    tests/nanocoap_cli: make test script paralelizable

--- tests/nanocoap_cli/test.sh
+++ tests/nanocoap_cli/test.sh
@@ -1,55 +1,82 @@
 #!/usr/bin/bash
 
-NUM=50
+NUM=36
 SEED=31337
 
 export BOARD=native
 export USE_ZEP=1
 
+n=0
+ELFFILE=bin/native/tests_nanocoap_cli.elf
+
+while [[ $# -gt 1 ]]; do
+  case $1 in
+    -n|--num)
+        NUM=$2
+        shift 2
+        ;;
+    -i|--id)
+        n=$2
+        shift 2
+        ;;
+    -e|--elf)
+        ELFFILE=$2
+        shift 2
+        ;;
+  esac
+done
+
+ZEP_PORT=$((17754 + n))
+SESSION=ZEPSession_$n
+PIDFILE=/tmp/$SESSION.pid
+
 case $1 in
 stop)
-    tmux kill-session -t ZEPSession
+    tmux kill-session -t $SESSION
     ;;
 init)
     reset
-    tmux kill-session -t ZEPSession
+    tmux kill-session -t $SESSION
 
     make -j all BOARD=native
 
-    ./topogen -w 25 -h 25 -r 10 -v 5 -n $NUM -s $SEED > test.topo
+#    ./topogen -w 250 -h 5 -r 10 -v 5 -n $NUM -s $SEED > test.topo
+     DIM=$(echo "4*sqrt($NUM)" | bc)
+    ./topogen -g -w $DIM -h $DIM -r 5 -v 0 -n $NUM -s $SEED > test.topo
+    tail -n $NUM test.topo | cut -d ' ' -f 2 | cut -f 1,2,3,4 > test.topo.map
 
-    tmux new-session -d -s ZEPSession ./zep_dispatch -s $SEED -t test.topo ::1 17754
+    tmux new-session -d -s $SESSION ./zep_dispatch -p $PIDFILE -s $SEED -t test.topo ::1 $ZEP_PORT
 
     for i in $(seq $NUM); do
-        MAC=$(for j in {1..8}; do echo -n "$i "; done | xargs | tr ' ' ':')
-        tmux set-option history-limit 5000 \; new-window -d -t ZEPSession: bin/native/tests_nanocoap_cli.elf --id=$i --eui64=$MAC -z [::1]:17754
+        MAC=$(printf "%x:%x:%x:%x:%x:%x:%x:%x" $i $i $i $i $i $i $i $i)
+        tmux set-option history-limit 8192 \; new-window -d -t $SESSION: $ELFFILE --id=$i --eui64=$MAC -z [::1]:$ZEP_PORT
     done
 
-    sleep 5
+    sleep 7
     ;;
 attach|a)
-    tmux a -t ZEPSession:$2
+    tmux a -t $SESSION:$2
     ;;
 start)
     echo Start Test
     START=$(date +%s.%m)
 
-    killall -SIGUSR2 zep_dispatch
-    tmux send-keys -t ZEPSession:1 "ncput_non /nvm0/dummy.txt /md5/in" Enter
+    kill -SIGUSR2 $(cat $PIDFILE)
+    tmux send-keys -t $SESSION:1 "ncput_non /nvm0/dummy.txt /md5/in" Enter
 
     for i in $(seq 2 $NUM); do
-        until HASH=$(tmux capture-pane -p -t ZEPSession:$i | grep hash); do sleep 1; done
+        until HASH=$(tmux capture-pane -p -t $SESSION:$i | grep hash); do sleep 1; done
         echo "$i| $HASH"
     done
 
-    killall -SIGUSR2 zep_dispatch
-    tmux capture-pane -p -t ZEPSession:0 | grep tx_total
+    kill -SIGUSR2 $(cat $PIDFILE)
+    tmux capture-pane -p -t $SESSION:0 | grep tx_total
     END=$(date +%s.%m)
     DIFF=$(echo "$END - $START" | bc)
     echo "finished in $DIFF seconds"
     ;;
 graph)
-    killall -USR1 zep_dispatch
+    kill -SIGUSR1 $(cat $PIDFILE)
     dot -Tpdf example.gv > example.gv.pdf
     ;;
 esac
