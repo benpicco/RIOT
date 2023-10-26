158b82ebf0085689acb7611d25bc204c0f38f0f8
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2023-10-26 12:46:37 +0200

    [test] examples/gnrc_networking: add firewall module

--- examples/gnrc_networking/main.c
+++ examples/gnrc_networking/main.c
@@ -26,12 +26,36 @@
 #define MAIN_QUEUE_SIZE     (8)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
+#include "net/gnrc/ipv6/firewall.h"
+GNRC_IPV6_FIREWALL_ADD(my_rule) = {
+    .flags = GNRC_IPV6_FW_FLAG_MATCH_SRC,
+    .prio = 42,
+};
+GNRC_IPV6_FIREWALL_ADD(my_rule2) = {
+    .flags = GNRC_IPV6_FW_FLAG_MATCH_DST,
+    .prio = 23,
+};
+GNRC_IPV6_FIREWALL_ADD(my_rule3) = {
+    .flags = GNRC_IPV6_FW_FLAG_MATCH_DST,
+    .prio = 17,
+};
+
+extern gnrc_ipv6_firewall_rule_t gnrc_ipv6_firewall_rules_const;
+extern gnrc_ipv6_firewall_rule_t gnrc_ipv6_firewall_rules_const_end;
+
 int main(void)
 {
     /* we need a message queue for the thread running the shell in order to
      * receive potentially fast incoming networking packets */
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
     puts("RIOT network stack example application");
+
+    printf("struct size: %u\n", sizeof(gnrc_ipv6_firewall_rule_t));
+    printf("gnrc_ipv6_firewall_rules_const: %p\n", (void *)&gnrc_ipv6_firewall_rules_const);
+    printf("my_rule: %p\n", (void *)&my_rule);
+    printf("my_rule2: %p\n", (void *)&my_rule2);
+    printf("my_rule3: %p\n", (void *)&my_rule3);
+    printf("gnrc_ipv6_firewall_rules_const_end: %p\n", (void *)&gnrc_ipv6_firewall_rules_const_end);
 
     /* start shell */
     puts("All up, running the shell now");
