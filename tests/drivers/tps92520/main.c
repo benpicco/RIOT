dcc1fdb33bfd2028ba274bede4034afdfac00a10
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2026-06-11 18:59:40 +0200

    fixup! tests/drivers/tps92520: add TPS92520 test app

--- tests/drivers/tps92520/main.c
+++ tests/drivers/tps92520/main.c
@@ -16,19 +16,38 @@
 
 #include <stdio.h>
 #include "tps92520.h"
+#include "ztimer.h"
 
 #include "macros/units.h"
 
 int main(void)
 {
     const tps92520_params_t params = {
-        .spi     = SPI_DEV(0),
-        .spi_clk = MHZ(1),
-        .cs_pin  = GPIO_PIN(PA, 13),
+        .spi     = SPI_DEV(1),
+        .spi_clk = KHZ(250),
+        .cs_pin  = GPIO_PIN(PC, 6),
     };
+
+    gpio_init(GPIO_PIN(PB, 0), GPIO_OUT);
+    gpio_init(GPIO_PIN(PB, 6), GPIO_OUT);
+    gpio_set(GPIO_PIN(PB, 6));
+    gpio_set(GPIO_PIN(PB, 0));
+
+    ztimer_sleep(ZTIMER_MSEC, 10);
 
     tps92520_t dev;
     int res = tps92520_init(&dev, &params);
+
+    int temp = tps92520_get_temperature(&dev);
+
+    printf("%d m°C\n", temp);
+
+    printf("5V: %d mV\n", tps92520_get_5V(&dev));
+    printf("Vin[0]: %d mV\n", tps92520_get_led_voltage(&dev, TPS92520_CH1VIN));
+    printf("Vin[1]: %d mV\n", tps92520_get_led_voltage(&dev, TPS92520_CH2VIN));
+
+    /* turn off chip */
+    gpio_clear(GPIO_PIN(PB, 0));
 
     return res;
 }
